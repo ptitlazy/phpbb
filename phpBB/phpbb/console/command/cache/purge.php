03307b87060589e71efc2e53cb7b68c9a829616b
LEZY Thomas <thomas.lezy@ensimag.grenoble-inp.fr>
2014-06-04 16:24:33 +0200

    [ticket/12657] Add a test file for base case
    PHPBB3-12657

--- phpBB/phpbb/console/command/cache/purge.php
+++ phpBB/phpbb/console/command/cache/purge.php
@@ -35,7 +35,7 @@ class purge extends \phpbb\console\command\command
 	/** @var \phpbb\config\config */
 	protected $config;
 
-	public function __construct(\phpbb\cache\driver\driver_interface $cache, \phpbb\db\driver\driver_interface $db, \phpbb\auth\auth $auth, \phpbb\log\log $log, \phpbb\user $user, \phpbb\config\config $config)
+	public function __construct(\phpbb\cache\driver\driver_interface $cache, \phpbb\db\driver\driver_interface $db, \phpbb\auth\auth $auth, \phpbb\log\log_interface $log, \phpbb\user $user, \phpbb\config\config $config)
 	{
 		$this->cache = $cache;
 		$this->db = $db;
